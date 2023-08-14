486cb9622781b9ffdac4654a66738bea17f5e3bb
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2023-08-14 20:59:11 -0300

    update required file on spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "psql_multi_ranges"
+require "activerecord-multirange"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
