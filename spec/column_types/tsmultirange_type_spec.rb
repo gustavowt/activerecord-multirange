d477c0c5af4bc7580244ce12c245b54e4acedb2b
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2023-08-21 20:08:53 -0300

    Enforcing UTC type

--- spec/column_types/tsmultirange_type_spec.rb
+++ spec/column_types/tsmultirange_type_spec.rb
@@ -3,25 +3,24 @@
 require "spec_helper"
 
 RSpec.describe "TsMultirangeType" do
+  let(:multiranges) { [10.days.ago.utc..9.days.ago.utc, 5.days.ago.utc...Time.current.utc] }
+
   it "initialize tsmultirange" do
-    multiranges = [10.days.ago..9.days.ago, 5.days.ago...Time.current]
     record = TestingRecord.new(column_ts: multiranges)
 
     expect(record.column_ts).to eq multiranges
   end
 
   it "create tsmultirange" do
-    multiranges = [10.days.ago..9.days.ago, 5.days.ago...Time.current]
     record = TestingRecord.create(column_ts: multiranges)
 
     expect(record.reload.column_ts).to eq multiranges
   end
 
   it "update tsmultirange" do
-    multiranges = [10.days.ago..9.days.ago, 5.days.ago...Time.current]
     record = TestingRecord.create(column_ts: multiranges)
 
-    new_multiranges = [1.week.from_now..2.weeks.from_now, 1.month.from_now..2.months.from_now]
+    new_multiranges = [1.week.from_now.utc..2.weeks.from_now.utc, 1.month.from_now.utc..2.months.from_now.utc]
     record.update(column_ts: new_multiranges)
 
     expect(record.reload.column_ts).to eq new_multiranges
