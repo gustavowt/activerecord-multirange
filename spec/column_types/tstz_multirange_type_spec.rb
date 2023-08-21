e2945210a7714ee7fc274e2fee1705e6823e04d1
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2023-08-21 20:40:34 -0300

    Add TsTzMultirange spec

--- spec/column_types/tstz_multirange_type_spec.rb
+++ spec/column_types/tstz_multirange_type_spec.rb
@@ -2,42 +2,42 @@
 
 require "spec_helper"
 
-RSpec.describe "TsMultirangeType" do
+RSpec.describe "TsTzMultirangeType" do
   let(:multiranges) do
     [
-      Time.parse("2022-05-05 09:30:00 UTC")...Time.parse("2022-06-06 16:30:00 UTC"),
-      Time.parse("2022-07-01 08:30:00 UTC")..Time.parse("2022-07-22 11:30:00 UTC"),
-      Time.parse("2022-10-01 11:30:00 UTC")...::Float::INFINITY
+      Time.parse("2022-05-05 09:30:00 PDT")...Time.parse("2022-06-06 16:30:00 UTC"),
+      Time.parse("2022-07-01 08:30:00 UTC")..Time.parse("2022-07-22 11:30:00 PDT"),
+      Time.parse("2022-10-01 11:30:00 -0200")...::Float::INFINITY
     ]
   end
 
   it "respond to correct type" do
-    expect(TestingRecord.new.column_for_attribute(:column_ts).type).to eq :tsmultirange
+    expect(TestingRecord.new.column_for_attribute(:column_tz).type).to eq :tstzmultirange
   end
 
   it "initialize tsmultirange" do
-    record = TestingRecord.new(column_ts: multiranges)
+    record = TestingRecord.new(column_tz: multiranges)
 
-    expect(record.column_ts).to eq multiranges
+    expect(record.column_tz).to eq multiranges
   end
 
   it "create tsmultirange" do
-    record = TestingRecord.create(column_ts: multiranges)
+    record = TestingRecord.create(column_tz: multiranges)
 
-    expect(record.reload.column_ts).to eq multiranges
+    expect(record.reload.column_tz).to eq multiranges
   end
 
   it "update tsmultirange" do
-    record = TestingRecord.create(column_ts: multiranges)
+    record = TestingRecord.create(column_tz: multiranges)
 
     new_multiranges = [
-      Time.parse("2021-05-05 09:30:00 UTC")...Time.parse("2021-06-06 16:30:00 UTC"),
-      Time.parse("2021-07-01 08:30:00 UTC")..Time.parse("2021-07-22 11:30:00 UTC"),
-      Time.parse("2021-10-01 11:30:00 UTC")...::Float::INFINITY
+      Time.parse("2021-05-05 09:30:00 -0300")...Time.parse("2021-06-06 16:30:00 -0500"),
+      Time.parse("2021-07-01 08:30:00 PDT")..Time.parse("2021-07-22 11:30:00 UTC"),
+      Time.parse("2021-10-01 11:30:00 -0600")...::Float::INFINITY
     ]
 
-    record.update(column_ts: new_multiranges)
+    record.update(column_tz: new_multiranges)
 
-    expect(record.reload.column_ts).to eq new_multiranges
+    expect(record.reload.column_tz).to eq new_multiranges
   end
 end
