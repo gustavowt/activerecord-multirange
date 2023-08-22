71a7b578c738f419e6cca933b91f810d6d07b8a8
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2023-08-21 21:13:46 -0300

    Add int8multirange type spec

---
+++ spec/column_types/int8multirange_type_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "int8multirangeType" do
+  let(:multiranges) do
+    [1...5, 12...300, 500...::Float::INFINITY]
+  end
+
+  it "respond to correct type" do
+    expect(TestingRecord.new.column_for_attribute(:column_int8).type).to eq :int8multirange
+  end
+
+  it "initialize int8multirange" do
+    record = TestingRecord.new(column_int8: multiranges)
+
+    expect(record.column_int8).to eq multiranges
+  end
+
+  it "create int8multirange" do
+    record = TestingRecord.create(column_int8: multiranges)
+
+    expect(record.reload.column_int8).to eq multiranges
+  end
+
+  it "update int8multirange" do
+    record = TestingRecord.create(column_int8: multiranges)
+
+    new_multiranges = [100...200, 700...::Float::INFINITY]
+
+    record.update(column_int8: new_multiranges)
+
+    expect(record.reload.column_int8).to eq new_multiranges
+  end
+end
